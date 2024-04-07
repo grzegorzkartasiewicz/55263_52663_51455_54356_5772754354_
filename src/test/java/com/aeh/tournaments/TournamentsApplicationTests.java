20071d188e36bbbf9425576deeef1e612a3471de
grzegorzkartasiewicz <kartasiewiczgrzegorz@gmail.com>
2024-04-07 10:51:22 +0200

    initial commit

---
+++ src/test/java/com/aeh/tournaments/TournamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeh.tournaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
